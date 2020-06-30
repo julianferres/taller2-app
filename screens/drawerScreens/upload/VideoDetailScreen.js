2ddf2c2ad14043a138ba1089cab1d7ee40947c8f
Uriel <urielkelman@gmail.com>
2020-06-29 21:06:01 -0300

    Solucion al problema del teclado y el video

--- screens/drawerScreens/upload/VideoDetailScreen.js
+++ screens/drawerScreens/upload/VideoDetailScreen.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import {StatusBar, View, TextInput, Text} from "react-native";
+import {StatusBar, View, TextInput, Text, TouchableOpacity, Button, Alert, KeyboardAvoidingView} from "react-native";
 import CustomHeader from "../../../navigation/CustomHeader";
 import { Video } from 'expo-av';
 import {connect} from 'react-redux';
@@ -13,20 +13,47 @@ class _VideoDetailScreen extends React.Component{
             locationFocused: false,
             title: "",
             description: "",
-            locatio: ""
+            location: ""
+        }
+    }
+
+    handleUpload() {
+        console.log(this.props.video)
+        if (this.state.title === "") {
+            Alert.alert(
+                "Empty title",
+                "You must provide a title for your video",
+                [{text: "Close", style: "cancel"}],
+                {cancelable: false}
+            )
+        } else if (this.state.description === "") {
+            Alert.alert(
+                "Empty description",
+                "You must provide a description for your video",
+                [{text: "Close", style: "cancel"}],
+                {cancelable: false}
+            )
+        } else if (this.state.location === "") {
+            Alert.alert(
+                "Empty location",
+                "You must provide a location for your video",
+                [{text: "Close", style: "cancel"}],
+                {cancelable: false}
+            )
+        } else {
+
         }
     }
 
     render() {
-        console.log(this.props.videoToUpload)
         return(
             <View style={{flex: 1, paddingTop: StatusBar.currentHeight}}>
                 <CustomHeader title="Video detail" navigation={this.props.navigation}/>
-                <View style={{paddingLeft: 30, paddingTop: 50, paddingRight: 50}}>
+                <View style={{paddingLeft: 30, paddingTop: 50, paddingRight: 30}}>
                     <Text style={{fontSize: 20}}>Title</Text>
                     <TextInput style={{paddingTop: 10, borderBottomWidth: 1,
                         borderBottomColor: this.state.titleFocused ? "#839FF9" : "lightblue"}}
-                               placeholder="Title"
+                               placeholder="Enter title"
                                onFocus={() => this.setState({titleFocused: true})}
                                onBlur={() => this.setState({titleFocused: false})}
                                onChangeText={(text) => this.setState({title: text})}
@@ -34,7 +61,7 @@ class _VideoDetailScreen extends React.Component{
                     <Text style={{fontSize: 20, paddingTop: 5}}>Description</Text>
                     <TextInput style={{paddingTop: 10, borderBottomWidth: 1,
                         borderBottomColor: this.state.descriptionFocused ? "#839FF9" : "lightblue"}}
-                               placeholder="Title"
+                               placeholder="Enter description"
                                onFocus={() => this.setState({descriptionFocused: true})}
                                onBlur={() => this.setState({descriptionFocused: false})}
                                onChangeText={(text) => this.setState({description: text})}
@@ -42,22 +69,33 @@ class _VideoDetailScreen extends React.Component{
                     <Text style={{fontSize: 20, paddingTop: 5}}>Location</Text>
                     <TextInput style={{paddingTop: 10, borderBottomWidth: 1,
                         borderBottomColor: this.state.locationFocused ? "#839FF9" : "lightblue"}}
-                               placeholder="Title"
+                               placeholder="Enter location"
                                onFocus={() => this.setState({locationFocused: true})}
                                onBlur={() => this.setState({locationFocused: false})}
                                onChangeText={(text) => this.setState({location: text})}
                     />
+                </View>
+                <KeyboardAvoidingView
+                    behavior={"height"}
+                    keyboardVerticalOffset={20}
+                    enabled={false}
+                    style={{flex: 1, justifyContent: "center", alignItems: "center", padding: 30}}>
                     <Video
                         source={{ uri: this.props.video.uri }}
-                        resizeMode={Video.RESIZE_MODE_CONTAIN}
+                        resizeMode={Video.RESIZE_MODE_STRETCH}
                         rate={1.0}
                         isMuted={true}
-                        shouldPlay
                         isLooping
                         useNativeControls
-                        style={{height: 200, width: 200}}
+                        style={{height: 300, width: 300}}
                     />
-                </View>
+                    <TouchableOpacity style={{padding: 30}}>
+                        <Button
+                            title="Upload!"
+                            onPress={() => this.handleUpload()}
+                        />
+                    </TouchableOpacity>
+                </KeyboardAvoidingView>
             </View>
 
         )
