7eb89717ed8f1b32485dd4f43acc2d377b1f7511
Julian Ferres <julianferres@gmail.com>
2020-07-01 20:16:06 -0300

    Agrego logout

--- navigation/SideBarNavigator.js
+++ navigation/SideBarNavigator.js
@@ -2,7 +2,7 @@ import * as React from 'react';
 import {createDrawerNavigator} from "@react-navigation/drawer"
 import BottomTabNavigator from "./BottomTabNavigator";
 import ProfileScreen from "../screens/drawerScreens/ProfileScreen";
-import CustomDrawer from "../screens/drawerScreens/CustomDrawer";
+import CustomDrawerContainer from "../screens/drawerScreens/CustomDrawer";
 import UploadVideoScreen from "../screens/drawerScreens/upload/UploadVideoScreen";
 import VideoDetailScreen from "../screens/drawerScreens/upload/VideoDetailScreen";
 
@@ -10,7 +10,7 @@ const Drawer = createDrawerNavigator();
 
 export default function SideBarNavigator(){
     return(
-        <Drawer.Navigator drawerContent={props => CustomDrawer(props)} >
+        <Drawer.Navigator drawerContent={() => <CustomDrawerContainer/>} >
             <Drawer.Screen name="TabMenu" component={BottomTabNavigator} />
             <Drawer.Screen name="Profile" component={ProfileScreen} />
             <Drawer.Screen name="UploadVideo" component={UploadVideoScreen} />
