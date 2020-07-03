c76f0077b5e8d2046014bfe67f9456928a7b1c81
Uriel <urielkelman@gmail.com>
2020-07-03 20:38:57 -0300

    Cambio intencional

--- screens/bottomScreens/chat/ChatScreen.js
+++ screens/bottomScreens/chat/ChatScreen.js
@@ -3,6 +3,7 @@ import {Text, TouchableOpacity, View, StatusBar} from 'react-native';
 import CustomHeader from "../../../navigation/CustomHeader";
 
 export default function ChatScreen({navigation}) {
+
   return (
       <View style={{flex: 1, paddingTop: StatusBar.currentHeight}}>
         <CustomHeader title="Messages" isHome={true} navigation={navigation}/>
