19d819f941f3e9bb0cdfcf223abcea9a2fd9f343
Julian Ferres <julianferres@gmail.com>
2020-07-08 12:18:48 -0300

    Agrego Horizontal lines a para separar likes de perfil de descripcion

--- screens/bottomScreens/video/VideoVisualizationScreen.js
+++ screens/bottomScreens/video/VideoVisualizationScreen.js
@@ -3,19 +3,55 @@ import {Dimensions, StatusBar, View, Text, ScrollView, Image, TouchableOpacity} 
 import CustomHeader from "../../../navigation/CustomHeader";
 import {Video} from "expo-av";
 import {connect} from "react-redux";
+import {AppLoading} from "expo";
+import * as Font from 'expo-font';
+
+let customFonts = {
+    "OpenSans": require('../../../assets/fonts/OpenSans-SemiBold.ttf'),
+    "OpenSans-regular": require('../../../assets/fonts/OpenSans-Regular.ttf')
+};
+const azulMarino = "#00335c";
+
+class HorizontalRule extends React.Component {
+    render() {
+        return (
+        <View
+            style={{
+                // borderBottomColor: azulMarino,
+                borderBottomColor:"#D2D2D2",
+                borderBottomWidth: 0.5,
+                paddingBottom:this.props.padding,
+                marginBottom:this.props.margin
+            }}
+        />);
+    }
+}
 
 class _VideoVisualizationScreen extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            isShowingCompleteDescription: false
+            isShowingCompleteDescription: false,
+            fontsLoaded: false,
         }
+    }
+
+    async _loadFontsAsync() {
+        await Font.loadAsync(customFonts);
+        this.setState({ fontsLoaded: true });
+    }
+    componentDidMount() {
+        this._loadFontsAsync();
     }
 
     render() {
         const widthResolution = Dimensions.get("window").width
         const heightResolution = widthResolution / 16 * 9;
+        if(!this.state.fontsLoaded){
+            return <AppLoading />
+        }
         return(
+
             <ScrollView style={{flex: 1, paddingTop: StatusBar.currentHeight}}>
                 <CustomHeader title="Watch" navigation={this.props.navigation}/>
                     <Video
@@ -29,7 +65,8 @@ class _VideoVisualizationScreen extends React.Component {
                         style={{width: widthResolution, height: heightResolution, backgroundColor: "black"}}
                     />
 
-                <Text style={{fontSize: 20, fontWeight: "bold", paddingLeft: 10, paddingRight: 10}}>{this.props.videoInfo.title}</Text>
+                <Text style={{fontSize: 25, fontWeight: "bold", paddingLeft: 10, paddingRight: 10, fontFamily:"OpenSans"}}>{this.props.videoInfo.title}</Text>
+                <HorizontalRule margin={0} padding={10}/>
                 <TouchableOpacity style={{flex: 1, flexDirection: "row", padding: 10, }}
                     onPress={() => this.props.navigation.navigate("UserProfile")}>
                     <Image source={{uri:`data:image/png;base64,${this.props.videoInfo.userPhoto}`}}
@@ -39,9 +76,12 @@ class _VideoVisualizationScreen extends React.Component {
                         <Text style={{fontWeight: "bold", fontSize: widthResolution / 25}}>{this.props.videoInfo.ownerName}</Text>
                     </View>
                 </TouchableOpacity>
+                <HorizontalRule margin={10} padding={0}/>
+                {/*<HorizontalRule />*/}
                 <TouchableOpacity onPress={() => this.setState({isShowingCompleteDescription: !this.state.isShowingCompleteDescription})}>
                     <Text numberOfLines={this.state.isShowingCompleteDescription ? 1000 : 5}
-                          style={{height: this.state.isShowingCompleteDescription ? "auto" : 84, paddingLeft: 10,
+                          style={{height: this.state.isShowingCompleteDescription ? "auto" : 100, paddingLeft: 10,
+                              fontFamily:"OpenSans-regular", fontSize:13, color:azulMarino,
                               paddingRight: 10}}>{this.props.videoInfo.description}</Text>
                 </TouchableOpacity>
             </ScrollView>
